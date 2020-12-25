c43e7d1a68d10b3cbf4b18f6e367b3d1bef06550
Tzur Paldi <tzur.paldi@earnix.com>
2020-12-25 22:02:46 +0200

    1. added instanceid 2. changed to async i/o

--- test/specs/test-demo.ts
+++ test/specs/test-demo.ts
@@ -1,9 +1,8 @@
-import randomize from "../../dev-dep/randomize";
 import {performancetotal} from "../../app";
 
-beforeEach(() => {
+beforeEach(async () => {
     console.log("Before hook");
-    performancetotal.initialize(true);
+    await performancetotal.initialize(false);
 });
 
 afterEach(() => {
@@ -20,25 +19,14 @@ describe('suite-1', () => {
     for (let i = 0; i < 3; i++) {
 
         it(`Test demo. Run ${i + 1}`, () => {
-            console.log("Test Started");
-
-            // Test Earnix website startup
+            console.log("Test started");
             performancetotal.sampleStart("Startup");
-            browser.url("https://earnix.com/");
+            
+            browser.url("https://github.com/");
+            
             performancetotal.sampleEnd("Startup");
-
-            // Test nested process
-            performancetotal.sampleStart("Wrap");
-
-            // Test weather API performance
-            performancetotal.sampleStart("WeatherAPI");
-            browser.url("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=xml");
-            performancetotal.sampleEnd("WeatherAPI");
-
-            // Simulate additional task inside 'Wrap'
-            browser.pause(1000 + randomize.RandomPlusMinus(200));
-
-            performancetotal.sampleEnd("Wrap");
+            
+            console.log("Test ended");
         });
     }
 });
