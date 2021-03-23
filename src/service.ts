e58e2b365ed59c8300eb97080f9cc630fba80bdb
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2021-03-23 19:13:08 +0100

    ISSUE-15: added cucumber specific hooks (#16)
    * ISSUE-15: added cucumber specific hooks
    
    * ISSUE-15: fixed passing of status
    
    * ISSUE-15: fix passing of status

--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,15 @@
 import performanceTotal from "./performance-total";
 
+enum Status {
+    UNKNOWN = 0,
+    PASSED = 1,
+    SKIPPED = 2,
+    PENDING = 3,
+    UNDEFINED = 4,
+    AMBIGUOUS = 5,
+    FAILED = 6
+}
+
 export default class PerformanceTotalService {
     browser: WebdriverIO.Browser;
     _serviceOptions: { disableAppendToExistingFile: boolean, performanceResultsFileName: string, dropResultsFromFailedTest: boolean, performanceResultsDirectory: string };
@@ -26,8 +36,22 @@ export default class PerformanceTotalService {
         await performanceTotal.initialize(this._serviceOptions.disableAppendToExistingFile, this._serviceOptions.performanceResultsDirectory);
     }
 
+    async beforeScenario(test: any, context: any) {
+        await performanceTotal.initialize(this._serviceOptions.disableAppendToExistingFile, this._serviceOptions.performanceResultsDirectory);
+    }
+
     afterTest(test: any, context: any, { error, result, duration, passed, retries }: any) {
         performanceTotal.finalize(passed);
+    }
+
+    afterScenario({ result }: any) {
+        let status = false;
+
+        if (result.passed === Status.PASSED) {
+            status = true;
+        }
+
+        performanceTotal.finalize(status);
     }
 
     async after(exitCode: any, config: any, capabilities: any) {
